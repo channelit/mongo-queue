48500c2f2d38a4fa79244b185c1e9b04699143bd
Hardik <hardikgw@gmail.com>
2019-11-06 15:13:56 -0500

    queue subscribe

--- README.md
+++ README.md
@@ -8,4 +8,6 @@
 ```shell script
 docker cp replica.js mongo1:/.
 docker exec mongo1 bash -c 'mongo < /replica.js'
+docker cp replica.js mongo2:/.
+docker exec mongo2 bash -c 'mongo --host localhost:27019 < /replica.js'
 ```
--- src/main/java/biz/cits/idepotent/App.java
+++ src/main/java/biz/cits/idepotent/App.java
@@ -3,24 +3,32 @@ package biz.cits.idepotent;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerAddress;
+import com.mongodb.connection.ClusterConnectionMode;
 import com.mongodb.connection.ClusterType;
+import com.mongodb.reactivestreams.client.ClientSession;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import com.mongodb.reactivestreams.client.MongoDatabase;
 import com.mongodb.selector.ServerSelector;
+import io.reactivex.Observable;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoRepositoriesAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication(exclude = {MongoReactiveAutoConfiguration.class, MongoReactiveRepositoriesAutoConfiguration.class, MongoAutoConfiguration.class, MongoRepositoriesAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class App {
 
     @Value("${db.mongo.host}")
@@ -48,15 +56,16 @@ public class App {
     public MongoClient mongoClient() {
 //        MongoCredential mongoCredential = MongoCredential.createCredential(DB_MONGO_USER, "admin", DB_MONGO_PSWD.toCharArray());
 
+        List<ServerAddress> hosts = Arrays.asList(new ServerAddress(DB_MONGO_HOST, 27017));
         MongoClient mongoClient = MongoClients.create(
                 MongoClientSettings.builder()
                         .applyToClusterSettings(builder ->
-                                builder.hosts(Arrays.asList(new ServerAddress(DB_MONGO_HOST, 27019)))
-                        .requiredClusterType(ClusterType.REPLICA_SET)
+                                builder.hosts(hosts).requiredReplicaSetName("fifo").requiredClusterType(ClusterType.REPLICA_SET)
                         )
-//                        builder.applyConnectionString(new ConnectionString("mongodb://localhost:27017,localhost:27019/?replicaSet=fifo&maxPoolSize=200")))
 //                        .credential(mongoCredential)
                         .build());
+        Observable<ClientSession> observable = Observable.fromPublisher(mongoClient.startSession());
+        observable.blockingFirst();
         return mongoClient;
     }
 
