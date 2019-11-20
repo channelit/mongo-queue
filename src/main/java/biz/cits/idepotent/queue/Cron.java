3ccebfa7ec70ee37a9cc19ba164ce35750f8a6f3
Hardik <hardikgw@gmail.com>
2019-11-20 09:46:09 -0500

    cron

--- src/main/java/biz/cits/idepotent/queue/Cron.java
+++ src/main/java/biz/cits/idepotent/queue/Cron.java
@@ -4,9 +4,11 @@ import biz.cits.idepotent.queue.subscriber.BaseSubscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+@Configuration
 @EnableScheduling
 public class Cron {
 
